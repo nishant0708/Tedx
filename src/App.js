47716bc44725abdec4c550cb3a494089c527ecad
Rachita Dashore <rachita17dashore@gmail.com>
2024-02-20 02:02:29 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Whyus from './Components/Whyus';
-import Footer from './Components/Footer';
+import Footer from './Components/Footer/Footer';
 // import Whyus2 from './Components/Whyus2';
 // import Flip from './Components/Flip';
 // import VisitCard from './Components/VisitCard';
 import './App.css';
-import HeroPg from './Components/HeroPg';
+import HeroPg from './Components/HeroPg/HeroPg';
 function App() {
   return (
     <div className='App'>
