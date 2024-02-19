14ef0a6042fbd8be88b2de87dfddc8bc00f7eafc
Rachita Dashore <rachita17dashore@gmail.com>
2024-02-20 01:44:47 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import Whyus from './Components/Whyus';
+import Footer from './Components/Footer';
+// import Whyus2 from './Components/Whyus2';
+// import Flip from './Components/Flip';
+// import VisitCard from './Components/VisitCard';
+import './App.css';
+import HeroPg from './Components/HeroPg';
+function App() {
+  return (
+    <div className='App'>
+      <HeroPg />
+      {/* <Flip /> */}
+      {/* <Whyus2/> */}
+  {/* <Hero /> */}
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          950: '#EB0028',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
