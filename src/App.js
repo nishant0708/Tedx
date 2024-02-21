cbb1cf5f0e0fe87a333194e17216053da9ff0c25
Rachita Dashore <rachita17dashore@gmail.com>
2024-02-21 23:02:41 +0530

    minorfix plus temp loader

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@
 import './App.css';
 import Forscrolldummy from './Components/Forscrolldummy';
 import HeroPg from './Components/HeroPg/HeroPg';
+import Loader from './Components/Loader/Loader';
 import Thoughtloom from './Components/thoughtloom/Thoughtloom';
-
+import React, { useState, useEffect } from 'react';
 function App() {
-  return (
-    <div className='App'>
-      <HeroPg />
-      {/* <Forscrolldummy/> */}
-      <Thoughtloom/>
-      {/* <Forscrolldummy/> */}
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // Simulating loading time
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
 
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const loadingIcon = document.querySelector('.loading-icon');
+      loadingIcon.classList.add('zoom');
+
+      // Remove the "zoom" class after animation completes
+      setTimeout(() => {
+        loadingIcon.classList.remove('zoom');
+      }, 2000);
+    }
+  }, [isLoading]);
+
+  return (
+    <div className="App">
+      <Loader isLoading={isLoading} />
+      {!isLoading && <div>
+        {/* <Loader /> */}
+        <HeroPg />
+        {/* <Forscrolldummy/> */}
+        <Thoughtloom />
+        {/* <Forscrolldummy/> */}</div>}
     </div>
+
   );
 }
 
