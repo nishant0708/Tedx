ae38aa8511d312a64c543f59287aeac097b6084f
Rachita Dashore <rachita17dashore@gmail.com>
2024-02-24 01:06:28 +0530

    change

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,14 +4,14 @@ import Speakerf from '../nik-pages/Speakerf'
 import Thoughtloom from '../thoughtloom/Thoughtloom'
 import StickyScroll from "../../pages/questions/faq"
 import FaqPage from '../../pages/questions/faqSection'
-
+// import Mehnat from "../jay mc/Mehnat";
 
 const Main = () => {
     
   return (
     <div>
         <HeroPg/>
- 
+    {/* <Mehnat /> */}
         <Thoughtloom/>
         <Speakerf/>
         <FaqPage/>
