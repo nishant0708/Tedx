67c7789e8d9eaaaad2dc242a067a92b0fdd373eb
Nishant Kaushal <nishantkaushal0708@gmail.com>
2024-02-23 15:08:35 +0530

    ab nhi hoga mujhse

--- src/Components/nik-pages/Speakerf.jsx
+++ src/Components/nik-pages/Speakerf.jsx
@@ -11,7 +11,7 @@ const Speakerf = () => {
 
     useEffect(() => {
         const handleResize = () => {
-          setIsSmallScreen(window.innerWidth < 830);
+          setIsSmallScreen(window.innerWidth < 1050);
         };
     
         handleResize(); // Initial check
